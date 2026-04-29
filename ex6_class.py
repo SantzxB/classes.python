0c1f1802c2ed6bbb721d23579393fac8ddd0c532
SantzxB <caiosantanaborges37@gmail.com>
2026-04-29 16:09:36 -0300

    Create ex6_class.py

---
+++ ex6_class.py
@@ -0,0 +1,27 @@
+import os
+
+os.system("cls")
+from dataclasses import dataclass
+
+@dataclass
+class Fornecedor:
+    nome:str
+    email:str
+    telefone:str
+    endereco:str
+    def mostrar_dados(self):
+        print(f"Nome: {fornecedor.nome}")
+        print(f"Email: {fornecedor.email}")
+        print(f"Telefone: (71){fornecedor.telefone}")
+        print(f"Endereço: {fornecedor.endereco}")
+
+print("\n Solicitando Dados do Fornecedor \n")
+
+fornecedor= Fornecedor(
+    nome=input("Nome: "), 
+    email=input("Email: "), 
+    telefone=input("Telefone: "),
+    endereco=input("Endereço: "))
+
+print("\n Exibindo Dados do Fornecedor \n")
+fornecedor.mostrar_dados()
